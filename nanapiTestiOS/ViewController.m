261a290e586250da1e7f54f815d284ecf51f3352
kozyty <kozyty@gmail.com>
2014-08-19 17:59:21 +0900

    Init commit.

--- nanapiTestiOS/ViewController.m
+++ nanapiTestiOS/ViewController.m
@@ -8,16 +8,44 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (strong, nonatomic) NSMutableArray *nanapiList;
 
 @end
+
+#define NANAPI_API_URL @"http://api.nanapi.jp/v1/recipeSearchDetails/?key=4b542e23e43f6&format=json&query=%E3%82%B5%E3%82%A4%E3%82%AF%E3%83%AA%E3%83%B3%E3%82%B0%20%E8%87%AA%E8%BB%A2%E8%BB%8A%20%E3%83%90%E3%82%A4%E3%82%AF"
 
 @implementation ViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // デリゲート初期化
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    // JSONの取得コール
+    [self getJson];
+}
+
+- (void)getJson
+{
+    NSURL *url = [NSURL URLWithString:NANAPI_API_URL];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        
+        // JSON形式のデータをNSDictへ
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        
+        // リスト管理するプロパティへ挿入
+        self.nanapiList = [[dict objectForKey:@"response"] objectForKey:@"recipes"];
+        
+        // データ取得後テーブルを再描画
+        [self.tableView reloadData];
+    }];
+     
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +54,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // 表示は3件のみ
+    return 3.f;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // セクションはいらない
+    return 1.f;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    NSDictionary *recipe = [self.nanapiList objectAtIndex:indexPath.row];
+    cell.textLabel.text = [recipe objectForKey:@"title"];
+    return cell;
+}
 @end
